1cb6e214f19b2104ba95db0bdee9168d8a7bbfbe
yoshio1949 <bump.satomi2331@gmail.com>
2023-06-23 14:53:46 +0900

    edit-pageを追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -20,6 +20,20 @@ class BoardsController < ApplicationController
     end
   end
 
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+    if @board.update(board_params)
+      redirect_to board_path(@board), notice: 'Updated'
+    else
+      flash.now[:error] = "Can't Update"
+      render :edit
+    end
+  end
+
   private
   def board_params
     params.require(:board).permit(:name, :description)
